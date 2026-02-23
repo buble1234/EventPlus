9ed23b830f5964c0d087ca6d6f36edd8ede69f9f
LuxeSystem <156671088+buble1234@users.noreply.github.com>
2026-02-23 10:04:26 +0300

    comit

--- src/main/java/event/plus/EventPlus.java
+++ src/main/java/event/plus/EventPlus.java
@@ -1,10 +1,51 @@
 package event.plus;
 
+import event.plus.core.EventBus;
+import event.plus.event.Event;
+import event.plus.event.impl.*;
 import net.fabricmc.api.ModInitializer;
 
-public class Eventplus implements ModInitializer {
+public class EventPlus implements ModInitializer {
 
     @Override
     public void onInitialize() {
+        sub(this);
+    }
+
+
+    public static void sub(Object obj) {
+        EventBus.sub(obj);
+    }
+
+    public static void unsub(Object obj) {
+        EventBus.unsub(obj);
+    }
+
+    public static <T extends Event> T fire(T event) {
+        return EventBus.fire(event);
+    }
+
+    public static boolean hasSubscribers(Class<? extends Event> eventType) {
+        return EventBus.hasSubscribers(eventType);
+    }
+
+    public static final class Events {
+        public static final Class<?> TICK = TickEvent.class;
+        public static final Class<?> RENDER_HUD = RenderHudEvent.class;
+        public static final Class<?> RENDER_WORLD = RenderWorldEvent.class;
+        public static final Class<?> MOVE = MoveEvent.class;
+        public static final Class<?> ATTACK = AttackEvent.class;
+        public static final Class<?> CHAT = ChatEvent.class;
+        public static final Class<?> PACKET = PacketEvent.class;
+        public static final Class<?> KEY = KeyEvent.class;
+        public static final Class<?> WORLD_LOAD = WorldLoadEvent.class;
+        public static final Class<?> JUMP = JumpEvent.class;
+        public static final Class<?> DEATH = DeathEvent.class;
+        public static final Class<?> SWING_HAND = SwingHandEvent.class;
+        public static final Class<?> MOUSE_CLICK = MouseClickEvent.class;
+        public static final Class<?> MOUSE_SCROLL = MouseScrollEvent.class;
+
+        private Events() {
+        }
     }
 }
--- src/main/java/event/plus/mixin/ClientConnectionMixin.java
+++ src/main/java/event/plus/mixin/ClientConnectionMixin.java
@@ -3,36 +3,23 @@ package event.plus.mixin;
 import event.plus.core.EventBus;
 import event.plus.event.impl.PacketEvent;
 import net.minecraft.network.ClientConnection;
-import net.minecraft.network.PacketCallbacks;
-import net.minecraft.network.listener.PacketListener;
 import net.minecraft.network.packet.Packet;
-import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(ClientConnection.class)
-public class PacketEventMixin {
+public class ClientConnectionMixin {
 
-    // Исходящий пакет
-    @Inject(
-            method = "send(Lnet/minecraft/network/packet/Packet;Lnet/minecraft/network/PacketCallbacks;)V",
-            at = @At("HEAD"),
-            cancellable = true
-    )
-    private void onSendPacket(Packet<?> packet, @Nullable PacketCallbacks callbacks, CallbackInfo ci) {
+    @Inject(method = "send(Lnet/minecraft/network/packet/Packet;)V", at = @At("HEAD"), cancellable = true)
+    private void onSendPacket(Packet<?> packet, CallbackInfo ci) {
         PacketEvent event = EventBus.fire(new PacketEvent(PacketEvent.Direction.SEND, packet));
         if (event.isCancelled()) ci.cancel();
     }
 
-    // Входящий пакет
-    @Inject(
-            method = "handlePacket",
-            at = @At("HEAD"),
-            cancellable = true
-    )
-    private static <T extends PacketListener> void onReceivePacket(Packet<T> packet, PacketListener listener, CallbackInfo ci) {
+    @Inject(method = "channelRead0", at = @At("HEAD"), cancellable = true)
+    private void onReceivePacket(io.netty.channel.ChannelHandlerContext ctx, Packet<?> packet, CallbackInfo ci) {
         PacketEvent event = EventBus.fire(new PacketEvent(PacketEvent.Direction.RECEIVE, packet));
         if (event.isCancelled()) ci.cancel();
     }
